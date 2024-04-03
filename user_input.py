1246f40c453c6ece88998b1e7d7b5373d3d4f759
Daniel Christopher Egbonyi <egbonyi.c.daniel@gmail.com>
2024-04-03 21:21:36 +0100

    first commit

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name = input('enter your name:')
+age = input('enter your age:')
+location = input('enter your location:')
+print( 'hello'+name+', your are '+ age + 'years old and live in' + location + '.') 
